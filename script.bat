2cb25ab90bf916785599004186cd6652098bd7b6
Matheus Oliveira <112136868+matheus-oliveira-venancio@users.noreply.github.com>
2023-12-09 02:48:52 -0300

    Add files via upload

---
+++ script.bat
@@ -0,0 +1,10 @@
+@echo off
+echo "------instalando apps------"
+color 0a
+winget install --id Microsoft.WindowsTerminal
+winget install --id Valve.Steam
+winget install --id  Google.Chrome
+winget install --id Opera.OperaGX
+winget install --id Neovim.Neovim
+winget install --id RARLab.WinRAR 
+echo "------instalando apps------"
